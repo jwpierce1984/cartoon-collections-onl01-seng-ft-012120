a4660eaf47ccdf77c5e0c6c1e5a3c5a925e0bbd9
Justin Pierce <jwpierce1984@gmail.com>
2020-01-31 23:04:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_long)
-  calls_long.any? {|i| i.length > 4}
-  calls_long.all? {|i| i.length <= 4}
+  calls_long.any? {|i| i.length < 4}
+  calls_long.all? {|i| i.length >= 4}
 end
 
 def find_the_cheese# code an argument here
