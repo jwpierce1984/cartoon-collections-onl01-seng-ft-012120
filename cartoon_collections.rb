b856e9e2253a4285949cd6bc0ae15664e7ad1642
Justin Pierce <jwpierce1984@gmail.com>
2020-01-31 23:05:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_long)
-  calls_long.any? {|i| i.length < 4}
+  calls_long.any? {|i| i.length >= 5}
   calls_long.all? {|i| i.length >= 4}
 end
 
