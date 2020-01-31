735db4ff3b9e52b08455af708224dcc3ba859e37
Justin Pierce <jwpierce1984@gmail.com>
2020-01-31 22:37:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.capitalize
+  planeteer_calls.each {|calls| calls.upcase}.join("! ")
   # Your code here
 end
 
