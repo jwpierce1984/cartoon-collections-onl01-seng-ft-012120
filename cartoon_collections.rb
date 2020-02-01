c0e245028445f7ff4602f32460ffd2250c264a21
Justin Pierce <jwpierce1984@gmail.com>
2020-02-01 01:45:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,7 @@ def long_planeteer_calls(calls_long)
   end
 
 def find_the_cheese(cheese)
+  cheese.include?
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
